aef477f1638ee3aec0fe781505f64cdfc8bda14d
Pavel Demianski <pasadem@ukr.net>
2021-02-21 02:55:46 +0300

    brain games

--- games/prime.js
+++ games/prime.js
@@ -1,36 +1,35 @@
 import getNum from '../src/getRundomNumber.js'
 
-
 const isEven = (num) => {
-    for (let i = 2; i < num; i += 1) {
-      if (num % i == 0) {
-        return false;
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) {
+      return false
     }
-  } return true;
-};
+  } return true
+}
 
-const num1 = getNum();  
-const num2 = getNum();  
-const num3 = getNum();  
+const num1 = getNum()
+const num2 = getNum()
+const num3 = getNum()
        
-const getGameData = () => {  
+const getGameData = () => {
   return {
     description: 'Answer "yes" if given number is prime. Otherwise answer "no".',
     rounds: [
-       {
-        question: num1, 
+      {
+        question: num1,
         answer: isEven(num1) ? 'yes' : 'no',
-       }, 
-       {
+      },
+      {
         question: num2, 
         answer: isEven(num2) ? 'yes' : 'no',
-       },
-       {
-        question: num3, 
+      },
+      {
+        question: num3,
         answer: isEven(num3) ? 'yes' : 'no',
-       }
-     ]
-   }
-};
-export default getGameData;
+      }
+    ]
+  }
+}
+export default getGameData
  
