938978bfd9f226336d5e86654c86180ae5f405eb
Pavel Demianski <pasadem@ukr.net>
2021-02-20 20:44:48 +0300

    last step

--- src/prime.js
+++ src/prime.js
@@ -1,21 +1,33 @@
-import getNum from ' ./getRundomNumber.js'
+import getNum from './getRundomNumber.js'
 
+const num = getNum();
+const isEven = (num) => {
+    for (let i = 2; i < num; i += 1) {
+      if (num % i == 0) {
+        return false;
+    }
+  } return true;
+};
 
+const num1 = getNum();  
+const num2 = getNum();  
+const num3 = getNum();  
+       
 const getGameData = () => {  
-   return {
-     description: 'Answer "yes" if given number is prime. Otherwise answer "no".',
-     rounds: [
+  return {
+    description: 'Answer "yes" if given number is prime. Otherwise answer "no".',
+    rounds: [
        {
-         question:  
-         answer: 
+        question: num1, 
+        answer: isEven(num1) ? 'yes' : 'no',
        }, 
        {
-        question: 
-        answer: 
+        question: num2, 
+        answer: isEven(num2) ? 'yes' : 'no',
        },
        {
-        question:  
-        answer: 
+        question: num3, 
+        answer: isEven(num3) ? 'yes' : 'no',
        }
      ]
    }
